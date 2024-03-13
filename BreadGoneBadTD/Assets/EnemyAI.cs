1a742aedf916da9711bfd771baa31ef38b5fcd75
JulianJ99 <julian99janssen@gmail.com>
2024-03-13 14:19:05 +0100

    Messy fix to SetDestination taking a few seconds when x = 0

--- BreadGoneBadTD/Assets/EnemyAI.cs
+++ BreadGoneBadTD/Assets/EnemyAI.cs
@@ -11,8 +11,12 @@ public class EnemyAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if(transform.position.x == 0.0f){
+            transform.position = new Vector3(0.1f, transform.position.y, transform.position.z);
+        }
 		agent.updateRotation = false;
-		agent.updateUpAxis = false; 
+		agent.updateUpAxis = false;
+        
     }
 
     // Update is called once per frame
