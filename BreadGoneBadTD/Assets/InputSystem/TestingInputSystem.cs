6124e02d063ea240a67dd35da045955bf7996785
ThomasDielesen <thomas.dielesen.gg@gmail.com>
2024-03-13 13:26:59 +0100

    Improved Movement system and Camera follow

---
+++ BreadGoneBadTD/Assets/InputSystem/TestingInputSystem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class TestingInputSystem : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    private Vector2 _moveDirection;
+
+    public InputActionReference move;
+
+    private void Update()
+    {
+        _moveDirection = move.action.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(_moveDirection.x * moveSpeed, _moveDirection.y * moveSpeed);
+    }
+}
