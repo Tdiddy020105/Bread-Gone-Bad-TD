f74deb567cbe4512fb1b8e9ca212cda922b1e162
JulianJ99 <julian99janssen@gmail.com>
2024-03-13 12:00:20 +0100

    2D map + enemies and NavMesh Plus added

---
+++ BreadGoneBadTD/Assets/EnemyAI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    [SerializeField] private UnityEngine.AI.NavMeshAgent agent;
+    [SerializeField] private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		agent.updateRotation = false;
+		agent.updateUpAxis = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null )
+        {
+            agent.SetDestination(target.position);    
+        }
+    }
+}
