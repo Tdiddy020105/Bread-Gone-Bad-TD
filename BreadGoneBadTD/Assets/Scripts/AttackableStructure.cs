2dd7ee6a2597d117b4a2313c9f6a6c542259a45c
Guylian Gilsing <gilsingguylian@gmail.com>
2024-03-13 21:46:21 +0100

    Configurable attack perimiter with debug lines

--- BreadGoneBadTD/Assets/Scripts/AttackableStructure.cs
+++ BreadGoneBadTD/Assets/Scripts/AttackableStructure.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class AttackableStructure : MonoBehaviour
 {
@@ -16,16 +18,59 @@ public class AttackableStructure : MonoBehaviour
 
     }
 
+    #region Debug line drawing
     private void OnDrawGizmos()
     {
         foreach (AttackPerimiter attackPerimiter in this.attackPerimiters)
         {
-            Gizmos.DrawLine(
-                new Vector3(this.transform.position.x - attackPerimiter.perimiterBounds.x, this.transform.position.y - attackPerimiter.perimiterBounds.y, 0f),
-                new Vector3(this.transform.position.x + attackPerimiter.perimiterBounds.x, this.transform.position.y - attackPerimiter.perimiterBounds.y, 0f)
-            );
+            this.DrawAttackPerimiterGizmo(attackPerimiter);
         }
     }
+
+    private void DrawAttackPerimiterGizmo(AttackPerimiter attackPerimiter)
+    {
+        Rect perimiterOutline = this.CalculateAttackPerimiterOutline(attackPerimiter);
+
+        Gizmos.color = Color.red;
+
+        // Top line
+        Gizmos.DrawLine(
+            new Vector3(perimiterOutline.x                         , perimiterOutline.y + perimiterOutline.height, 0f),
+            new Vector3(perimiterOutline.x + perimiterOutline.width, perimiterOutline.y + perimiterOutline.height, 0f)
+        );
+
+        // Right line
+        Gizmos.DrawLine(
+            new Vector3(perimiterOutline.x + perimiterOutline.width, perimiterOutline.y                          , 0f),
+            new Vector3(perimiterOutline.x + perimiterOutline.width, perimiterOutline.y + perimiterOutline.height, 0f)
+        );
+
+        // Bottom line
+        Gizmos.DrawLine(
+            new Vector3(perimiterOutline.x                         , perimiterOutline.y, 0f),
+            new Vector3(perimiterOutline.x + perimiterOutline.width, perimiterOutline.y, 0f)
+        );
+
+        // Left line
+        Gizmos.DrawLine(
+            new Vector3(perimiterOutline.x, perimiterOutline.y                          , 0f),
+            new Vector3(perimiterOutline.x, perimiterOutline.y + perimiterOutline.height, 0f)
+        );
+    }
+
+    private Rect CalculateAttackPerimiterOutline(AttackPerimiter attackPerimiter)
+    {
+        float halfWidth = (this.transform.localScale.x / 2) + attackPerimiter.perimiterBounds.x;
+        float cornerLeftX = this.transform.localPosition.x - halfWidth;
+        float cornerRightX = this.transform.localPosition.x + halfWidth;
+
+        float halfHeight = (this.transform.localScale.y / 2) + attackPerimiter.perimiterBounds.y;
+        float cornerTopY = this.transform.localPosition.y - halfHeight;
+        float cornerBottomY = this.transform.localPosition.y + halfHeight;
+
+        return new Rect(cornerLeftX, cornerTopY, cornerRightX - cornerLeftX, cornerBottomY - cornerTopY);
+    }
+    #endregion
 }
 
 [Serializable]
