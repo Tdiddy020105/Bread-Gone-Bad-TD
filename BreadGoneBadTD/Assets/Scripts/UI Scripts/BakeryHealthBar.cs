88d2f75eebe2fd6fddb4e7528d7927fa33fe9223
Wesley2315 <477269@student.fontys.nl>
2024-03-15 14:06:22 +0100

    healthbar at top left

--- BreadGoneBadTD/Assets/Scripts/UI Scripts/BakeryHealthBar.cs
+++ BreadGoneBadTD/Assets/Scripts/UI Scripts/BakeryHealthBar.cs
@@ -12,8 +12,8 @@ public class BakeryHealthBar : MonoBehaviour
     void Start()
     {
         healthBar.transform.Find("BakeryHealthGreen");
-        life = 40f;     // remove when Daphne's bakery script is implemented
-        // life = bakery.GetComponent<BakeryHealth>.GetHitPoints();           // Daphne implementation
+        life = 40f;
+        // life = bakery.GetComponent<AttackableStructure>.GetHealth();
         maxLife = life;
         healthBar.localScale = new Vector3(life / maxLife, 1f);
     }
@@ -26,13 +26,11 @@ public class BakeryHealthBar : MonoBehaviour
             healthBar.localScale = new Vector3(life / maxLife, 1f);
         }
         /*
-         * else if (life != bakery.GetComponent<BakeryHealth>.GetHitPoints() && life > 0)
+         * else if (life != bakery.GetComponent<AttackableStructure>.GetHealth() && life > 0)
          * {
-         *  life = bakery.GetComponent<BakeryHealth>.GetHitPoints();
+         *  life = bakery.GetComponent<AttackableStructure>.GetHealth();
          *  healthBar.localScale = new Vector3(life / maxLife, 1f);
          * }
-         * 
-         * Once Daphne gets health to work for bakery
         */
     }
 }
