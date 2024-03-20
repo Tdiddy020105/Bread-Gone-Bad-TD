8d3287cf6134b3d8ee59544f9e8f90b38001ff4d
Wesley2315 <477269@student.fontys.nl>
2024-03-20 16:58:17 +0100

    fix healthbar

--- BreadGoneBadTD/Assets/Scripts/UI Scripts/BakeryHealthBar.cs
+++ BreadGoneBadTD/Assets/Scripts/UI Scripts/BakeryHealthBar.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BakeryHealthBar : MonoBehaviour
 {
     [SerializeField] private GameObject bakery;
-    [SerializeField] private Transform healthBar;
+    [SerializeField] private Image healthBar;
     private float life;
     private float maxLife;
 
@@ -14,7 +15,6 @@ public class BakeryHealthBar : MonoBehaviour
         life = 40f;
         // life = bakery.GetComponent<AttackableStructure>().GetHealth();
         maxLife = life;
-        healthBar.localScale = new Vector3(life / maxLife, 1f);
     }
 
     void Update()
@@ -22,14 +22,14 @@ public class BakeryHealthBar : MonoBehaviour
         if (life <= 0)
         {
             life = 0;
-            healthBar.localScale = new Vector3(life / maxLife, 1f);
+            healthBar.fillAmount = life / maxLife;
         }
         /*
-         * else if (life != bakery.GetComponent<AttackableStructure>().GetHealth() && life > 0)
-         * {
-         *  life = bakery.GetComponent<AttackableStructure>().GetHealth();
-         *  healthBar.localScale = new Vector3(life / maxLife, 1f);
-         * }
+        else if (life != bakery.GetComponent<AttackableStructure>().GetHealth() && life > 0)
+        {
+            life = bakery.GetComponent<AttackableStructure>().GetHealth();
+            healthBar.fillAmount = life / maxLife;
+        }
         */
     }
 }
