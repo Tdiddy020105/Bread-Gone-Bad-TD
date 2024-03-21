e541ba21614fdf865ae2dcb2756a816baf6c5732
Brian Hutjens <57947324+YZBrian@users.noreply.github.com>
2024-03-21 14:47:10 +0100

    Fixed map generation and added tower placement
    Code is jank and ducktape, will look into that later.
    Things that still need attention:
    - Tower logic
    - Custom Inspector Script (place Tiles)
    - Tower Placement shouldn't be in Tile Logic.

--- BreadGoneBadTD/Assets/Scripts/Map Scripts/Map.cs
+++ BreadGoneBadTD/Assets/Scripts/Map Scripts/Map.cs
@@ -4,19 +4,47 @@ using UnityEngine;
 
 public class Map : MonoBehaviour
 {
-    [SerializeField] private int width;
-    [SerializeField] private int height;
+    [SerializeField]
+    [Tooltip("Sets the width of the generated grid.")]
+    [Range(1, 100)]
+    private int width;
 
+    [SerializeField]
+    [Tooltip("Sets the height of the generated grid.")]
+    [Range(1, 100)]
+    private int height;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Tile placeholderTile;
+    [SerializeField] private Tile grassTile;
+    [SerializeField] private Tile gravelTile;
+
+    private Dictionary<Vector2, Tile> tiles;
+
+    public void GenerateGrid()
     {
+        tiles = new Dictionary<Vector2, Tile>();
 
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                var randomTile = Random.Range(0, 6) == 3 ? gravelTile : grassTile;
+                var spawnedTile = Instantiate(grassTile, new Vector3(x*4, y*4), Quaternion.identity, this.transform);
+                spawnedTile.name = $"Tile {x+1} {y+1}";
+
+                spawnedTile.Init(x, y);
+
+                tiles[new Vector2(x, y)] = spawnedTile;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DestroyGrid()
     {
-
+        GameObject[] allTiles = GameObject.FindGameObjectsWithTag("Tile");
+        foreach (GameObject tile in allTiles)
+        {
+            DestroyImmediate(tile);
+        }
     }
 }
