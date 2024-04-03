1f50661962f0cb2e27669080a8421c1460bbbdce
Guylian Gilsing (School Laptop) <78902@roc-teraa.nl>
2024-04-03 12:06:09 +0200

    Added GetAllTiers method to IUpgrades so that UI can display all upgrade tiers

--- BreadGoneBadTD/Assets/Scripts/Upgrade Scripts/IUpgrades.cs
+++ BreadGoneBadTD/Assets/Scripts/Upgrade Scripts/IUpgrades.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
+using UnityEngine;
 
-public interface IUpgrades<T>
+public interface IUpgrades<T> where T : ScriptableObject
 {
     public bool AllTiersUnlocked();
     public List<T> GetUnlockedTiers();
+    public List<UpgradeTier<T>> GetTiers();
 }
