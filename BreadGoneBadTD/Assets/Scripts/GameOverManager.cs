d831dfb4f8aa717065aee88ec82aa77043d97a42
Guylian Gilsing <gilsingguylian@gmail.com>
2024-04-16 10:15:54 +0200

    Game over state: wave number persistence between scenes

--- BreadGoneBadTD/Assets/Scripts/GameOverManager.cs
+++ BreadGoneBadTD/Assets/Scripts/GameOverManager.cs
@@ -1,9 +1,59 @@
+using DesignPatterns.EnemyPool;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverManager : MonoBehaviour
 {
+    private WaveManager waveManager = null;
+    private static GameOverState gameOverState = null;
+
+    private void Start()
+    {
+        // Retrieve the wave manager when no game over state has been set
+        if (gameOverState == null)
+        {
+            this.waveManager = GameObject.Find("WaveManager")?.GetComponent<WaveManager>();
+
+            if (this.waveManager == null)
+            {
+                throw new System.Exception("Game over manager broken, can't find \"WaveManager\" game object.");
+            }
+
+            return;
+        }
+
+        // Update game over menu and reset game over state
+        GameOverMenu gameOverMenu = this.GetComponent<GameOverMenu>();
+
+        if (gameOverMenu == null)
+        {
+            throw new System.Exception("Game over manager broken, can't find \"GameOverMenu\" script.");
+        }
+
+        int receivedCurrency = gameOverState.wave;
+
+        gameOverMenu.UpdateWaveNumber(gameOverState.wave);
+        gameOverMenu.UpdateReceivedPermanentCurrency(receivedCurrency);
+
+        CurrencyManager.Instance.Earn(receivedCurrency, CurrencyType.PERMANENT);
+
+        gameOverState = null;
+    }
+
     public void AttackableStructureDestroyed()
     {
-        Debug.Log("GAME OVER NOOB!!!");
+        gameOverState = new GameOverState(this.waveManager.GetWaveNumber());
+        SceneManager.LoadScene("GameOver");
+    }
+}
+
+public class GameOverState
+{
+    public int wave;
+
+    public GameOverState() {}
+    public GameOverState(int wave)
+    {
+        this.wave = wave;
     }
 }
--- BreadGoneBadTD/Assets/Scripts/UI Scripts/GameOverMenu.cs
+++ BreadGoneBadTD/Assets/Scripts/UI Scripts/GameOverMenu.cs
@@ -1,10 +1,30 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverMenu : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI waveNumber;
+    [SerializeField] private TextMeshProUGUI permanentCurrencyReceivedNumber;
+
+    private void Start()
+    {
+        this.waveNumber.text = "0";
+        this.permanentCurrencyReceivedNumber.text = "+0";
+    }
+
     public void GoToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void UpdateWaveNumber(int number)
+    {
+        this.waveNumber.text = number.ToString();
+    }
+
+    public void UpdateReceivedPermanentCurrency(int receivedCurrencyAmount)
+    {
+        this.permanentCurrencyReceivedNumber.text = $"+{receivedCurrencyAmount}";
     }
 }
